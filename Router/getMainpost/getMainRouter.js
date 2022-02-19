1d3d1eefc2990fff6df2def08cb1ed8e9692ee2c
ShinHyoung <jsin2475@naver.com>
2022-02-19 21:25:06 +0900

    modify : because of PostPage all folder's name has changed

--- Router/getMainpost/getMainRouter.js
+++ Router/getMainpost/getMainRouter.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var router = express.Router();
-const pool = require("../../database/database");
 const getPost = require("./getPost");
 
 router.post("/", async (req, res) => {
--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const bodyParser = require("body-parser");
 const port = 8080;
 const cors = require("cors");
 const setPostRouter = require("./Router/setPost/setPostRouter");
-const getPostRouter = require("./Router/getPost/getPostRouter");
+const getMainRouter = require("./Router/getMainpost/getMainRouter");
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/getpost", getPostRouter);
+app.use("/getMainpost", getMainRouter);
 app.use("/setpost", setPostRouter);
 
 app.listen(port, () => {
