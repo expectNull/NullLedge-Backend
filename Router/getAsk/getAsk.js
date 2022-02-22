6aa23ef05c767f93d31839e1dae3a8359e15b857
ShinHyoung <jsin2475@naver.com>
2022-02-22 21:46:48 +0900

    modify : getPostHeader -> getAsk

--- Router/getAsk/getAsk.js
+++ Router/getAsk/getAsk.js
@@ -3,8 +3,6 @@ const getLike = require("../getLike/getLike");
 const timeConvert = require("../timeConverter");
 
 async function getItem(item, id) {
-  let post_id = item.POST_ID;
-
   let ret = {
     post_nm: item.POST_NM,
     post_ymd: timeConvert(item.POST_YMD),
@@ -13,27 +11,25 @@ async function getItem(item, id) {
     view_cnt: item.VIEW_CNT,
     like_cnt: await getLike(id),
     user_nm: item.USER_NICK_NM,
-    np_amt: item.NULLPOINT_ANT,
+    user_np: item.NULLPOINT_AMT,
+    user_status: item.STATUS_CONTENT,
   };
 
   return ret;
 }
 
-async function getPost(id) {
-  // type_gb = 0으로 질문글 확인, order by를 통해 최신글 부터
-  var sql = `select POST_NM, POST_YMD, POST_TAGS_NM, CONTENT, VIEW_CNT, USER_NICK_NM, NULLPOINT_AMT
+async function getAsk(id) {
+  var sql = `select POST_NM, POST_YMD, POST_TAGS_NM, CONTENT, 
+  VIEW_CNT, USER_NICK_NM, NULLPOINT_AMT, STATUS_CONTENT
   from POST_TB join USER_TB on POST_TB.USER_ID = USER_TB.USER_ID
    where post_ID = ${id};`;
-  let ret = [];
 
   let connection = await pool.getConnection(async conn => conn);
   let [rows, col] = await connection.query(sql);
 
-  ret.push(await getItem(rows[0], id));
-
   connection.release();
 
-  return ret;
+  return await getItem(rows[0], id);
 }
 
-module.exports = getPost;
+module.exports = getAsk;
