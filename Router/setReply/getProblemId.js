ac186993d7b8429daf5e89c4d7905463ef6e0170
LONGNEW <jsin2475@naver.com>
2022-03-01 00:55:44 +0900

    modify : changing query to parameterized...

--- Router/setReply/getProblemId.js
+++ Router/setReply/getProblemId.js
@@ -1,9 +1,12 @@
 const pool = require("../../database/database");
 
 async function getProblemId(parent_id) {
-  var sql = `SELECT PROBLEM_ID as ProId FROM POST_TB WHERE POST_ID = ${parent_id}`;
+  var sql = `SELECT PROBLEM_ID as ProId FROM POST_TB WHERE POST_ID = ?`;
+  let params = parent_id;
+
   let connection = await pool.getConnection(async conn => conn);
-  let [rows, col] = await connection.query(sql);
+  let [rows, col] = await connection.query(sql, params);
+
   let ret = rows[0].ProId;
   connection.release();
 
