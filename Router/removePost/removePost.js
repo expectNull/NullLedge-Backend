37009681ec6c76baf623d39f313545cf8f582fcb
박병휘 <201802093@o.cnu.ac.kr>
2022-03-18 17:11:15 +0900

    mod : removePost

--- Router/removePost/removePost.js
+++ Router/removePost/removePost.js
@@ -2,14 +2,19 @@ const pool = require("../../database/database");
 
 async function removePost(post_id) {
   // type_gb = 0으로 질문글 확인, order by를 통해 최신글 부터
-  var sql = `delete from LIKE_LOG_TB WHERE POST_ID = ?;
-  delete from TAG_TB WHERE POST_ID = ?;
-  delete from POST_TB WHERE POST_ID = ?;`;
+  var sql = [
+    `delete from LIKE_LOG_TB WHERE POST_ID = ?;`,
+    `delete from TAG_TB WHERE POST_ID = ?;`,
+    `delete from POST_TB WHERE POST_ID = ?;`,
+  ];
 
-  let params = [post_id, post_id, post_id];
+  let params = [post_id];
 
   let connection = await pool.getConnection(async conn => conn);
-  let [rows, col] = await connection.query(sql, params);
+
+  for (let i = 0; i < sql.length; i++) {
+    let [rows, col] = await connection.query(sql[i], params);
+  }
 
   connection.release();
 }
