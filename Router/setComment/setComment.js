831140978afdadc5c50672855de66992fd86ce93
dblepart99 <minbmw8205@gmail.com>
2022-03-28 17:23:46 +0900

    modify : time problem

--- Router/setComment/setComment.js
+++ Router/setComment/setComment.js
@@ -4,14 +4,14 @@ const { getUserId } = require("../getUserId");
 
 async function setComment(info) {
   var ProId = 0;
-  var sql = `INSERT INTO POST_TB(PROBLEM_ID, USER_ID, TYPE_GB, POST_YMD, CONTENT, PARENT_POST_ID, CHECK_GB) 
+  var sql = `INSERT INTO POST_TB(PROBLEM_ID, USER_ID, TYPE_GB, CONTENT, PARENT_POST_ID, CHECK_GB) 
   VALUES(?, ?, ?, ?, ?, ?, ?);`;
   info.user_id = await getUserId(info.user_token);
   let params = [
     ProId,
     info.user_id,
     info.type_gb,
-    new Date().toISOString().split(".")[0],
+    // new Date().toISOString().split(".")[0],
     info.comment,
     info.parent_post_id,
     2,
--- Router/setReply/setReply.js
+++ Router/setReply/setReply.js
@@ -5,14 +5,14 @@ const { getUserId } = require("../getUserId");
 async function setReply(info) {
   var ProId = await getProblemId(info.parent_post_id);
   info.html_content = info.html_content.replace(/"/gi, '\\"');
-  var sql = `INSERT INTO POST_TB(PROBLEM_ID, USER_ID, TYPE_GB, POST_YMD, CONTENT, PARENT_POST_ID, CHECK_GB) 
+  var sql = `INSERT INTO POST_TB(PROBLEM_ID, USER_ID, TYPE_GB, CONTENT, PARENT_POST_ID, CHECK_GB) 
   VALUES(?, ?, ?, ?, ?, ?, ?);`;
   info.user_id = await getUserId(info.user_token);
   let params = [
     ProId,
     info.user_id,
     info.type_gb,
-    new Date().toISOString().split(".")[0],
+    // new Date().toISOString().split(".")[0],
     info.html_content,
     info.parent_post_id,
     2,
